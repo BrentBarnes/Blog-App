728e770cd482f6b15bf7963000bddcadfa8213c9
Brent Barnes <BrenttBarness@gmail.com>
2022-07-19 12:45:21 -0400

    finished blog app 1.0

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+
+  get "/pages/:page" => "pages#show"
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{html,js,erb}"],
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '786px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        grayGreen: '#7F867B',
+        lightGreen: '#C7C7BB',
+        superLightGreen: '#E3E4E0',
+        brown: '#AF9F8C',
+        lightPink: '#D4C8BE'
+      },
+    },
   },
-  plugins: [],
 }
